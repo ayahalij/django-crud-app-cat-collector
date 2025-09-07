52c7e95cb2d79e88f9c044f3f79ace04aeb0c9d6
ayahalij <ayahali2003@gmail.com>
2025-09-07 17:58:08 +0300

    catcollecter

---
+++ main_app/views.py
@@ -0,0 +1,51 @@
+
+
+
+# main_app/views.py
+
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat 
+
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
+
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+# views.py
+def cat_index(request):
+    cats = Cat.objects.all()  # look familiar?
+    return render(request, 'cats/index.html', {'cats': cats})
+
+
+# Create a list of Cat instances
+
+# views.py
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+
+# views.py
+# main-app/views.py
+
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
